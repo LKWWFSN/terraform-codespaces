bd068ae7f8fbc3efe8998d4729a16997cb8fa42f
Friedrich Nemec <nemec@walter-group.com>
2026-06-18 10:17:53 +0000

    add

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+
+variable "repository_name" {
+  description = "The name of the GitHub repository"
+  type        = string
+}
+
+
+variable "repository_description" {
+  description = "Description of the repository"
+  type        = string
+}
+
+variable "repository_visibility" {
+  description = "Visibility of the repository"
+  type        = string
+}
